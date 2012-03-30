2091bcb074a78c8f250a8c6c1b63403f47bce7c8
Daniel James <dwhjames@gmail.com>
2012-03-30 15:29:38 +0100

    Change XMLVisitor to output SVG

--- smallerdraw/src/uk/ac/ox/softeng/dpa/smallerdraw/visitors/XMLVisitor.java
+++ smallerdraw/src/uk/ac/ox/softeng/dpa/smallerdraw/visitors/XMLVisitor.java
@@ -15,12 +15,12 @@ import javax.xml.stream.XMLStreamWriter;
 import uk.ac.ox.softeng.dpa.smallerdraw.Figure;
 import uk.ac.ox.softeng.dpa.smallerdraw.Picture;
 import uk.ac.ox.softeng.dpa.smallerdraw.Visitor;
-import uk.ac.ox.softeng.dpa.smallerdraw.figures.ConnectingLineFigure;
 import uk.ac.ox.softeng.dpa.smallerdraw.figures.GroupFigure;
 import uk.ac.ox.softeng.dpa.smallerdraw.figures.LineFigure;
 import uk.ac.ox.softeng.dpa.smallerdraw.figures.OvalFigure;
 import uk.ac.ox.softeng.dpa.smallerdraw.figures.PolygonFigure;
 import uk.ac.ox.softeng.dpa.smallerdraw.figures.RectangleFigure;
+import uk.ac.ox.softeng.dpa.smallerdraw.util.RectangleUtils;
 
 /**
  * A vistor that writes XML to an OutputStream.
@@ -33,6 +33,7 @@ import uk.ac.ox.softeng.dpa.smallerdraw.figures.RectangleFigure;
 public class XMLVisitor implements Visitor {
 
 	private XMLStreamWriter xmlWriter;
+	private static final String fillColor = "#D3D3D3";
 
 	public XMLVisitor(OutputStream outputStream) {
 		Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream,
@@ -59,11 +60,20 @@ public class XMLVisitor implements Visitor {
 	public void visit(Picture picture) {
 		try {
 			xmlWriter.writeStartDocument();
-			xmlWriter.writeStartElement("picture");
-
+			xmlWriter.writeDTD("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">");
+			xmlWriter.writeStartElement("svg");
+			xmlWriter.writeAttribute("xmlns", "http://www.w3.org/2000/svg");
+			xmlWriter.writeAttribute("version", "1.1");
+			
+			xmlWriter.writeStartElement("g");
+			xmlWriter.writeAttribute("stroke", "black");
+			xmlWriter.writeAttribute("stroke-width", "2px");
+			
 			for (Figure figure : picture.figures()) {
 				figure.accept(this);
 			}
+			
+			xmlWriter.writeEndElement();
 
 			xmlWriter.writeEndElement();
 			xmlWriter.writeEndDocument();
@@ -75,7 +85,7 @@ public class XMLVisitor implements Visitor {
 	@Override
 	public void visit(GroupFigure groupFigure) {
 		try {
-			xmlWriter.writeStartElement("group");
+			xmlWriter.writeStartElement("g");
 
 			for (Figure figure : groupFigure.getSubFigures()) {
 				figure.accept(this);
@@ -90,13 +100,11 @@ public class XMLVisitor implements Visitor {
 	@Override
 	public void visit(LineFigure lineFigure) {
 		try {
-			xmlWriter.writeStartElement("line");
-			if (lineFigure instanceof ConnectingLineFigure) {
-				xmlWriter.writeAttribute("connecting", "true");
-			}
-			writePoint(lineFigure.getStart());
-			writePoint(lineFigure.getEnd());
-			xmlWriter.writeEndElement();
+			xmlWriter.writeEmptyElement("line");
+			xmlWriter.writeAttribute("x1", String.valueOf(lineFigure.getStart().x));
+			xmlWriter.writeAttribute("y1", String.valueOf(lineFigure.getStart().y));
+			xmlWriter.writeAttribute("x2", String.valueOf(lineFigure.getEnd().x));
+			xmlWriter.writeAttribute("y2", String.valueOf(lineFigure.getEnd().y));
 		} catch (XMLStreamException ex) {
 			ex.printStackTrace();
 		}
@@ -105,9 +113,14 @@ public class XMLVisitor implements Visitor {
 	@Override
 	public void visit(OvalFigure ovalFigure) {
 		try {
-			xmlWriter.writeStartElement("oval");
-			writeRectangle(ovalFigure.getBounds());
-			xmlWriter.writeEndElement();
+			xmlWriter.writeEmptyElement("ellipse");
+			Rectangle r = ovalFigure.getBounds();
+			Point c = RectangleUtils.getCenter(r);
+			xmlWriter.writeAttribute("cx", String.valueOf(c.x));
+			xmlWriter.writeAttribute("cy", String.valueOf(c.y));
+			xmlWriter.writeAttribute("rx", String.valueOf(r.width / 2));
+			xmlWriter.writeAttribute("ry", String.valueOf(r.height / 2));
+			writeFilledAttribute(ovalFigure);
 		} catch (XMLStreamException ex) {
 			ex.printStackTrace();
 		}
@@ -116,11 +129,14 @@ public class XMLVisitor implements Visitor {
 	@Override
 	public void visit(PolygonFigure polygonFigure) {
 		try {
-			xmlWriter.writeStartElement("polygon");
+			xmlWriter.writeEmptyElement("polygon");
+			StringBuilder builder = new StringBuilder();
 			for (int i = 0; i < polygonFigure.getSize(); i++) {
-				writePoint(polygonFigure.getPoint(i));
+				Point p = polygonFigure.getPoint(i);
+				builder.append(p.x).append(',').append(p.y).append(' ');
 			}
-			xmlWriter.writeEndElement();
+			xmlWriter.writeAttribute("points", builder.toString());
+			writeFilledAttribute(polygonFigure);
 		} catch (XMLStreamException ex) {
 			ex.printStackTrace();
 		}
@@ -129,31 +145,25 @@ public class XMLVisitor implements Visitor {
 	@Override
 	public void visit(RectangleFigure rectangleFigure) {
 		try {
-			xmlWriter.writeStartElement("rectangle");
-			writeRectangle(rectangleFigure.getBounds());
-			xmlWriter.writeEndElement();
-		} catch (XMLStreamException ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	private void writePoint(Point p) {
-		try {
-			xmlWriter.writeEmptyElement("point");
-			xmlWriter.writeAttribute("x", String.valueOf(p.x));
-			xmlWriter.writeAttribute("y", String.valueOf(p.y));
-		} catch (XMLStreamException ex) {
-			ex.printStackTrace();
-		}
-	}
-
-	private void writeRectangle(Rectangle r) {
-		try {
 			xmlWriter.writeEmptyElement("rect");
+			Rectangle r = rectangleFigure.getBounds();
 			xmlWriter.writeAttribute("x", String.valueOf(r.x));
 			xmlWriter.writeAttribute("y", String.valueOf(r.y));
-			xmlWriter.writeAttribute("w", String.valueOf(r.width));
-			xmlWriter.writeAttribute("h", String.valueOf(r.height));
+			xmlWriter.writeAttribute("width", String.valueOf(r.width));
+			xmlWriter.writeAttribute("height", String.valueOf(r.height));
+			writeFilledAttribute(rectangleFigure);
+		} catch (XMLStreamException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private void writeFilledAttribute(Figure figure) {
+		try {
+			if (figure.isFilled()) {
+				xmlWriter.writeAttribute("fill", fillColor);
+			} else {
+				xmlWriter.writeAttribute("fill", "none");
+			}
 		} catch (XMLStreamException ex) {
 			ex.printStackTrace();
 		}
